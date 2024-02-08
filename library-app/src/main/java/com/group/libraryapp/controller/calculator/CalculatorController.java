137e0da02e0ad8f82a5029ba51ab92bc9264c14b
yeoung_huns <vosxja1@gmail.com>
2024-02-08 10:05:59 +0900

    POST로 넘어온 HTTPBody를 객체로 변환해서 받아보기

--- library-app/src/main/java/com/group/libraryapp/controller/calculator/CalculatorController.java
+++ library-app/src/main/java/com/group/libraryapp/controller/calculator/CalculatorController.java
@@ -1,7 +1,10 @@
 package com.group.libraryapp.controller.calculator;
 
 import com.group.libraryapp.dto.calculator.request.CalculatorAddRequest;
+import com.group.libraryapp.dto.calculator.request.CalculatorMultipleRequest;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,5 +14,11 @@ public class CalculatorController {
     @GetMapping("/add") //get/add
     public int  addTwoNumbers(CalculatorAddRequest request){
         return request.getNumber1() + request.getNumber2();
+    }
+
+
+    @PostMapping("/multiply") //POSt/multiply
+    public int myltiplyTwoNumbers(@RequestBody CalculatorMultipleRequest request){ //post 는 @RequestBody로 JSON을받아서 객체로 변환한다.
+        return request.getNumber1() * request.getNumber2();
     }
 }
