695bd7e65ea71c1f219f3b991e5337dd31d6ed1d
yeoung_huns <vosxja1@gmail.com>
2024-02-08 10:37:00 +0900

    Get과 Post로 간단한 유저 생성/조회 API 생성해보기

---
+++ library-app/src/main/java/com/group/libraryapp/dto/user/response/UserResponse.java
@@ -0,0 +1,32 @@
+package com.group.libraryapp.dto.user.response;
+
+import com.group.libraryapp.domain.User;
+
+public class UserResponse {
+    private final long id;
+    private final String name;
+    private final Integer age;
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public UserResponse(long id, String name, Integer age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+    public UserResponse(long id, User user){
+        this.id = id;
+        this.name = user.getName();
+        this.age = user.getAge();
+    }
+}
