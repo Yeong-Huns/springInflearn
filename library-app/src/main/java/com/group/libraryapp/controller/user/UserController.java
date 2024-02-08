b024540c01a8751a24f75b084f42dc4859506596
yeoung_huns <vosxja1@gmail.com>
2024-02-08 12:10:11 +0900

    레이어드 아키텍처(Controller/Service/Repository)구조로 기능별 레이어 분리

--- library-app/src/main/java/com/group/libraryapp/controller/user/UserController.java
+++ library-app/src/main/java/com/group/libraryapp/controller/user/UserController.java
@@ -3,6 +3,7 @@ package com.group.libraryapp.controller.user;
 import com.group.libraryapp.dto.user.request.UserCreateRequest;
 import com.group.libraryapp.dto.user.request.UserUpdateRequest;
 import com.group.libraryapp.dto.user.response.UserResponse;
+import com.group.libraryapp.service.user.UserService;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,48 +11,29 @@ import java.util.List;
 
 @RestController
 public class UserController {
-
-    private final JdbcTemplate jdbcTemplate;
-
+    private final UserService service;
     //생성자에 넣어두면 스프링이 알아서 넣어줌
     public UserController(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
+        this.service = new UserService(jdbcTemplate);
     }
 
     @PostMapping("/user")
     public void saveUser(@RequestBody UserCreateRequest request) {
-        String sql = "INSERT INTO user (name, age) VALUES (?, ?)";
-        jdbcTemplate.update(sql, request.getName(), request.getAge());
+        service.saveUser(request);
     }
 
     @GetMapping("/user")
     public List<UserResponse> getUsers() {
-        String sql = "SELECT * FROM user";
-        return jdbcTemplate.query(sql, (rs, rowNum) -> {
-            long id = rs.getLong("id");
-            String name = rs.getString("name");
-            int age = rs.getInt("age");
-            return new UserResponse(id, name, age);
-        });
+        return service.getUsers();
     }
 
     @PutMapping("/user")
     public void updateUser(@RequestBody UserUpdateRequest request){
-        String readSql = "SELECT * FROM user WHERE id=?";
-        boolean isUserNotExist = jdbcTemplate.query(readSql, (rs, rowNum) -> 0, request.getId()).isEmpty();
-        //필드가 존재하지않는다면 Empty / 있으면 0이 반환된다.
-        if(isUserNotExist) {throw new IllegalArgumentException();}
-        String sql = "UPDATE user SET name=? WHERE id=?";
-        jdbcTemplate.update(sql, request.getName(), request.getId());
+        service.updateUser(request);
     }
 
     @DeleteMapping("/user")
     public void deleteUser(@RequestParam String name){
-        String readSql = "SELECT * FROM user WHERE name=?";
-        boolean isUserNotExist = jdbcTemplate.query(readSql, (rs, rowNum) -> 0, name).isEmpty();
-        //필드가 존재하지않는다면 Empty / 있으면 0이 반환된다.
-        if(isUserNotExist) {throw new IllegalArgumentException();}
-        String sql = "DELETE FROM user WHERE name=?";
-        jdbcTemplate.update(sql, name);
+        service.deleteUser(name);
     }
 }
---
+++ library-app/src/main/java/com/group/libraryapp/repository/user/UserRepository.java
@@ -0,0 +1,51 @@
+package com.group.libraryapp.repository.user;
+
+import com.group.libraryapp.dto.user.request.UserCreateRequest;
+import com.group.libraryapp.dto.user.request.UserUpdateRequest;
+import com.group.libraryapp.dto.user.response.UserResponse;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.List;
+
+public class UserRepository {
+    private final JdbcTemplate jdbcTemplate;
+
+    public UserRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public void saveUser(UserCreateRequest request){
+        String sql = "INSERT INTO user (name, age) VALUES (?, ?)";
+        jdbcTemplate.update(sql, request.getName(), request.getAge());
+    }
+
+    public List<UserResponse> getUsers(){
+        String sql = "SELECT * FROM user";
+        return jdbcTemplate.query(sql, (rs, rowNum) -> {
+            long id = rs.getLong("id");
+            String name = rs.getString("name");
+            int age = rs.getInt("age");
+            return new UserResponse(id, name, age);
+        });
+    }
+
+    public void updateUser(UserUpdateRequest request){
+        String sql = "UPDATE USER SET name=? WHERE id=?";
+        jdbcTemplate.update(sql, request.getName(), request.getId());
+    }
+
+    public void deleteUser(String name){
+        String sql = "DELETE FROM user WHERE name=?";
+        jdbcTemplate.update(sql, name);
+    }
+
+    public boolean isUserNotExist(long id){
+        String sql = "SELECT * FROM user WHERE id=?";
+        return jdbcTemplate.query(sql, (rs, rowNum) -> 0, id).isEmpty();
+    }
+    public boolean isUserNotExist(String name){
+        String sql = "SELECT * FROM user WHERE name=?";
+        return jdbcTemplate.query(sql, (rs, rowNum) -> 0, name).isEmpty();
+    }
+
+}
